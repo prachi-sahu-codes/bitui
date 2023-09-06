8b42a9b4c9065851ec3cfa91527ef092b2987ce2
prachi-sahu-codes <prachisahu998@gmail.com>
2023-09-07 01:15:44 +0530

    chore: index updated

---
+++ src/components/index.jsx
@@ -0,0 +1,11 @@
+import Alert from "./Alert";
+import Avatar from "./Avatar";
+import Badge from "./Badge";
+import Button from "./Button";
+import Card from "./Card";
+import Headings from "./Headings";
+import Image from "./Image";
+import Text from "./Text";
+
+
+export {Alert, Avatar, Badge, Card, Button, Headings, Image, Text};
